0f252b4ca2457c600028eae250eda71034a80b22
gorgbus <63368174+gorgbus@users.noreply.github.com>
2022-04-22 17:49:24 +0200

    fixed db err

--- src/database/schemas/Channel.ts
+++ src/database/schemas/Channel.ts
@@ -22,7 +22,6 @@ const ChannelSchema = new Schema<channel>({
         {
             type: mongoose.SchemaTypes.ObjectId,
             ref: "members",
-            unique: true
         }
     ],
     guild: {
