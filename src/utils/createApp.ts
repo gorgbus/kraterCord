8e0c0ba081b9368cf644970ddfdb27e95cc59b5a
gorgbus <63368174+gorgbus@users.noreply.github.com>
2022-04-23 15:40:54 +0200

    proxy

--- src/utils/createApp.ts
+++ src/utils/createApp.ts
@@ -27,6 +27,7 @@ function createApp(): Express {
         secret: process.env.SESSION_SECRET!,
         resave: false,
         saveUninitialized: false,
+        proxy: true,
         cookie: {
             maxAge: 60000 * 60 * 24 * 7,
             sameSite: PROD ? "none" : "lax",
