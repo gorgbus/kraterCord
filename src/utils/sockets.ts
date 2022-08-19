8856934139be9f2e8117e44a276b6cc9a897027c
gorgbus <63368174+gorgbus@users.noreply.github.com>
2022-08-19 15:48:30 +0000

    moved ms to separate server

--- src/utils/sockets.ts
+++ src/utils/sockets.ts
@@ -3,10 +3,6 @@ import Channel from "../database/schemas/Channel";
 import Member, { member } from "../database/schemas/Member";
 import Notif from "../database/schemas/Notif";
 import mongoose from "mongoose";
-import { createRouter, createWorker } from "./worker";
-import { Producer, WebRtcTransport, Consumer } from "mediasoup/node/lib/types";
-import { createWebRtcTrans } from "./createWebrtcTrans";
-import { transports, producer, consumer, rooms, peers } from "./types";
 
 interface ISocket extends Socket {
     user?: string;
@@ -14,20 +10,7 @@ interface ISocket extends Socket {
 
 const sockets = new Map<string, string[]>();
 
-let rooms: rooms = {};
-let peers: peers = {};
-
-let transports: transports[] = [];
-let producers: producer[] = [];
-let consumers: consumer[] = [];
-
 const socketIo = async (io: Server) => {
-    try {
-        await createWorker();
-    } catch (err) {
-        console.log(err);
-    }
-
     io.on("connection", (s: ISocket) => {
         console.log(`Socket: ${s.id} has connected`);
 
@@ -50,124 +33,6 @@ const socketIo = async (io: Server) => {
 
         s.on("status", (user: member, id: string) => {
             io.to(id).emit("online", user);
-        });
-
-        s.on("ms_setup", async (channel, callback) => {
-            const router = await createRoom(channel, s.id);
-
-            peers[s.id] = {
-                socket: s,
-                channel,
-                transports: [],
-                consumers: [],
-                producers: [],
-                user: s.user!,
-            }
-
-            s.join(channel);
-
-            callback({ rtpCapabilities: router.rtpCapabilities });
-        });
-
-        s.on("crt_trans", async ({ consumer }, callback) => {
-            const { channel } = peers[s.id];
-            const router = rooms[channel].router;
-
-            createWebRtcTrans(router).then((transport) => {
-                callback({
-                    dtlsParameters: transport.dtlsParameters, 
-                    id: transport.id, 
-                    iceParameters: transport.iceParameters, 
-                    iceCandidates: transport.iceCandidates
-                });
-
-                addTransport(transport, channel, s.id, consumer);
-            }, error => {
-                console.log(error);
-            });
-        });
-
-        s.on("con_trans", async (dtlsParameters, consumer, id?) => {
-            getTransport(s.id, consumer, id).connect({ dtlsParameters });
-        });
-
-        s.on("produce", async (kind, rtpParameters, consumer, callback) => {
-            const producer = await getTransport(s.id, consumer).produce({ kind, rtpParameters })
-
-            const { channel } = peers[s.id];
-
-            addPruducer(producer, channel, s.id);
-
-            s.to(channel).emit("new_producer", producer.id, s.user!);
-
-            callback({ 
-                id: producer.id,
-                producerExists: producers.length > 1 ? true : false
-            });
-        });
-
-        s.on("get_producers", (callback) => {
-            const { channel } = peers[s.id];
-
-            let producerList: {id: string; userId: string;}[] = [];
-            producers.forEach(pd => {
-                if (pd.id !== s.id && pd.channel === channel) {
-                    producerList = [...producerList, { id: pd.producer.id, userId: peers[pd.id].user }];
-                }
-            });
-
-            callback(producerList);
-        });
-
-        s.on("consume", async (rtpCapabilities, producerId, transportId, callback) => {
-            try {
-                console.log(producerId);
-
-                const { channel } = peers[s.id];
-                const router = rooms[channel].router;
-
-                const consumerTrans = transports.find(td => td.consumer && td.transport.id == transportId)?.transport;
-
-                if (router.canConsume({
-                    producerId,
-                    rtpCapabilities
-                })) {
-                    const consumer = await consumerTrans?.consume({ rtpCapabilities, paused: true, producerId });
-
-                    if (!consumer) return;
-
-                    consumer.on("producerclose", () => {
-                        consumerTrans?.close();
-                        transports = transports.filter(td => td.transport.id !== consumerTrans?.id);
-                        consumer.close();
-                        consumers = consumers.filter(cd => cd.consumer.id !== consumer.id);
-                    });
-
-                    addConsumer(consumer!, channel, s.id);
-
-                    callback({
-                        producerId: producerId,
-                        id: consumer.id,
-                        kind: consumer.kind,
-                        rtpParameters: consumer.rtpParameters,
-                        type: consumer.type,
-                    });
-                }
-            } catch (err) {
-                console.error(err);
-            }
-        });
-
-        s.on('pause', async (id, pause) => {
-            const producer = producers.find(p => p.producer.id === id)?.producer;
-
-            pause ? await producer?.pause() : await producer?.resume();
-        });
-
-        s.on("resume", async (id) => {
-            const consumer = consumers.find(c => c.consumer.id === id)?.consumer;
-
-            await consumer?.resume();
         });
 
         s.on("update_user", async (user: member) => {
@@ -286,47 +151,11 @@ const socketIo = async (io: Server) => {
             });
         });
 
-        s.on('swapping_media', (callback) => {
-            consumers = removeItems(consumers, s.id, "consumer");
-            producers = removeItems(producers, s.id, "producer");
-            transports = removeItems(transports, s.id, "transport");
-
-            if (!peers[s.id]) return callback();
-
-            const { channel } = peers[s.id];
-
-            rooms[channel] = {
-                router: rooms[channel].router,
-                peers: rooms[channel].peers.filter(peer => peer !== s.id),
-            }
-
-            delete peers[s.id];
-
-            callback();
-        });
-
         s.on("leave_channel", async (id, user) => {
             try {
                 await Channel.findByIdAndUpdate(id, { $pull: { users: { user } } });
 
                 s.broadcast.emit("user_disconnected", id, user);
-
-                await s.leave(id);
-                
-                consumers = removeItems(consumers, s.id, "consumer");
-                producers = removeItems(producers, s.id, "producer");
-                transports = removeItems(transports, s.id, "transport");
-
-                if (!peers[s.id]) return;
-
-                const { channel } = peers[s.id];
-
-                rooms[channel] = {
-                    router: rooms[channel].router,
-                    peers: rooms[channel].peers.filter(peer => peer !== s.id),
-                }
-
-                delete peers[s.id];
             } catch (err) {
                 console.log(err);
             }
@@ -337,21 +166,6 @@ const socketIo = async (io: Server) => {
             if (io.sockets.adapter.rooms.get(`${s.user}-status`)) return;
 
             const user = await Member.findByIdAndUpdate(s.user, { $set: { status: "offline" } }, { new: true });
-
-            consumers = removeItems(consumers, s.id, "consumer");
-            producers = removeItems(producers, s.id, "producer");
-            transports = removeItems(transports, s.id, "transport");
-
-            if (peers[s.id]) {
-                const { channel } = peers[s.id];
-
-                delete peers[s.id];
-
-                rooms[channel] = {
-                    router: rooms[channel].router,
-                    peers: rooms[channel].peers.filter(peer => peer !== s.id),
-                }
-            }
 
             s.broadcast.emit("online", user);
 
@@ -366,84 +180,6 @@ const socketIo = async (io: Server) => {
             console.log(`Socket: ${s.id} has disconnected`);
         });
     });
-}
-
-const createRoom = async (roomName: string, socketId: string) => {
-    let router;
-    let peers: string[] = [];
-
-    if (rooms[roomName]) {
-        router = rooms[roomName].router
-        peers = rooms[roomName].peers || []
-    } else {
-        router = await createRouter();
-    }
-
-    rooms[roomName] = {
-        router,
-        peers: [...peers, socketId]
-    }
-
-    return router;
-}
-
-const addTransport = (transport: WebRtcTransport, channel: string, id: string, consumer: boolean) => {
-    transports = [
-        ...transports,
-        { id, transport, channel, consumer }
-    ]
-
-    peers[id] = {
-        ...peers[id],
-        transports: [
-           ...peers[id].transports,
-           transport.id, 
-        ]
-    }
-}
-
-const addPruducer = (producer: Producer, channel: string, id: string) => {
-    producers = [
-        ...producers,
-        { id, producer, channel }
-    ]
-
-    peers[id] = {
-        ...peers[id],
-        producers: [
-            ...peers[id].producers,
-            producer.id
-        ]
-    }
-}
-
-const addConsumer = (consumer: Consumer, channel: string, id: string) => {
-    consumers = [
-        ...consumers,
-        { id, consumer, channel }
-    ]
-
-    peers[id] = {
-        ...peers[id],
-        consumers: [
-            ...peers[id].consumers,
-            consumer.id
-        ]
-    }
-}
-
-const getTransport = (id: string, consumer: boolean, transId?: string) => transId ? transports.filter(transport => transport.id === id && transport.consumer === consumer && transport.transport.id === transId)[0].transport : transports.filter(transport => transport.id === id && transport.consumer === consumer)[0].transport;
-
-const removeItems = (items: Array<any>, id: string, type: string) => {
-    items.forEach(item => {
-        if (item.id === id) {
-            item[type].close();
-        }
-    });
-
-    items = items.filter(item => item.id !== id);
-
-    return items;
 }
 
 const emitToUser = (id: string, io: Server, event: string, ...args: any) => {
